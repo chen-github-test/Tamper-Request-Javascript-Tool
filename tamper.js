04e8ebc9b389600fca360a2bbbc4aee1ee8b0737
Casper <chen.bjut@foxmail.com>
2022-09-29 09:42:00 +0800

    testtest

--- tamper.js
+++ tamper.js
@@ -1,11 +1,7 @@
 /* 
 * ###################################################################
 * ##### https://github.com/ttodua/Tamper-Request-Javascript-Tool2 ####
-<<<<<<< HEAD
-* ### Don't put comments inside body when using the script inline ### 
-=======
 * ### Don't put comments inside body when using the script inline test optimize_function change file ### 
->>>>>>> optimize_function
 * ###################################################################
 */
 
